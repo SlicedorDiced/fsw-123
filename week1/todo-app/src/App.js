1abeaa83142cc227846dbdfd19fc5c23f3cd61e4
SlicedorDiced <Joshua.luna-perez@bryanuniversity.edu>
2022-05-26 13:03:17 -0400

    week1 todo app

---
+++ week1/todo-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ListItems from "./Listitems"
+
+function App() {
+  return (
+
+  <ListItems/>
+  
+  );
+}
+
+export default App;
---
+++ week1/todo-app/src/Listitems.js
@@ -0,0 +1,28 @@
+
+export default function ListItems(){
+
+    return(
+        <div>
+        <h3>List Of Todos</h3>
+        <ul>
+          <li>
+            <input type="checkbox"></input>
+            <span>Food Shopping</span>
+          </li>
+          <li>
+            <input type="checkbox"></input>
+            <span>Car Wash</span>
+          </li>
+          <li>
+            <input type="checkbox"></input>
+            <span>Walk Pet</span>
+          </li>
+          <li>
+            <input type="checkbox"></input>
+            <span>Study</span>
+          </li>
+        </ul>
+      </div>
+    );
+}
+
